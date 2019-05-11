847938f1b5804f1be37a9d1b9420d644f0da655a
luis fernando cifuentes <luis.fernando.cifuentes@correounivalle.edu.co>
2019-05-10 20:20:01 -0500

    test person

--- main_test.go
+++ main_test.go
@@ -1,10 +1,19 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/golang/example/stringutil"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	total := Sum(5, 1)
+	total := Sum(5, 9)
 	if total != 10 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
+}
+
+func ExampleReverse() {
+	fmt.Println(stringutil.Reverse("hello_"))
+	// Output: olleh
 }
